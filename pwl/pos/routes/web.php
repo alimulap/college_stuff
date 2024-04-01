2570239e4d0f371d63f1fa41c6bad01c06c349a8
alimulap <mohammad4limul@gmail.com>
2024-04-01 20:29:29 +0700

    praktikum 5

--- pwl/pos/routes/web.php
+++ pwl/pos/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TransactionController;
@@ -18,4 +19,6 @@ Route::get('/user/{id}/name/{name}', [UserController::class, 'index']);
 Route::get('/transactions', [TransactionController::class, 'transactions']);
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
+
 
