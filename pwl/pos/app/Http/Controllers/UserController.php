aea0944a41671af64e0db86addd2b39934eed085
alimulap <mohammad4limul@gmail.com>
2024-04-02 21:02:31 +0700

    jobsheet 4 praktikum 2.2

--- pwl/pos/app/Http/Controllers/UserController.php
+++ pwl/pos/app/Http/Controllers/UserController.php
@@ -36,9 +36,12 @@ class UserController extends Controller
         //$user = UserModel::where('level_id', 1)->first();
         //$user = UserModel::firstWhere('level_id', 1);
 
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
+        //$user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //    abort(404);
+        //});
+
+        //$user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'managet9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
