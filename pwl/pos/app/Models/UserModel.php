143bd56957ceb88151f7fb4e056c8fdf14c28b27
alimulap <mohammad4limul@gmail.com>
2024-05-26 03:56:28 +0700

    Jobsheet 11 praktikum

--- pwl/pos/app/Models/UserModel.php
+++ pwl/pos/app/Models/UserModel.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 //use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -22,10 +24,23 @@ class UserModel extends Authenticatable implements JWTSubject
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = [
+        'level_id',
+        'username',
+        'nama',
+        'password',
+        'image',
+    ];
 
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
     }
 }
--- pwl/pos/routes/api.php
+++ pwl/pos/routes/api.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 // })->middleware('auth:sanctum');
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/register1', App\Http\Controllers\Api\RegisterController::class)->name('register1');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
